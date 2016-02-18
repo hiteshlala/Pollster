dc8c0c96c0d976cc729bf2dde7bcb1beaed55503
Oliver <oliverbhuang@gmail.com>
2016-02-18 15:57:11 -0800

    Created sql tables via sequelize

--- server/db/test.sql
+++
@@ -1,6 +0,0 @@
-insert into users (name, email, password) values ('michael', 'somehere@nowhere', 'whocares'), ('hitesh', 'bigOG@OGsOnly.com', 'iAmTheOG')
-
--- insert into events (name, private, start, end, creator) values ('OG-Meetup', 1, '2016-02-17 17:00', '2016-02-17 18:00', 'hitesh')
-
-
--- insert into events (name, private, start, end, creator) values ('gentlemens-club-event', 1, '2016-02-17 17:00', '2016-02-17 19:00', 'steffin')
