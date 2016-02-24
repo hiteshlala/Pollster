576da2b4938840a225bc6691d883fa7b50742d5e
Oliver <oliverbhuang@gmail.com>
2016-02-24 11:01:50 -0800

    refactored eventsController to pollsController

--- server/controller/pollsController.js
+++ server/controller/pollsController.js
@@ -2,7 +2,7 @@ var db = require('../db');
 
 module.exports = {
   post: function (req, res, next) {
-    db.models.Event.create({
+    db.models.Poll.create({
       name: req.body.name,
       private: req.body.private,
       start: req.body.start,
@@ -16,8 +16,8 @@ module.exports = {
 
   put: function (req, res, next) {
     console.log(req.body);
-    db.models.UserEvent.create({
-      EventId: req.body.eventId,
+    db.models.UserPoll.create({
+      PollId: req.body.pollId,
       UserId: req.body.userId
     })
     .then(function (success) {
@@ -25,24 +25,24 @@ module.exports = {
     });
   },
 
-  // recieves a user id and returns all events they are in
+  // recieves a user id and returns all polls they are in
   get: function(req, res, next) {
-    // query UserEvent for all events IDS assossicated with the id
-    db.models.UserEvent.findAll({
+    // query Userpoll for all polls IDS assossicated with the id
+    db.models.UserPoll.findAll({
       where: {
         UserId: req.params.userid
       }
     })
     .then(function(result) {
-      var eventIdList = result.map(function(pair){ return pair.EventId; });
-      return db.models.Event.findAll({
+      var pollIdList = result.map(function(pair){ return pair.PollId; });
+      return db.models.Poll.findAll({
         where : {
-          id: { $in: eventIdList }
+          id: { $in: pollIdList }
         }
       });
     })
-    .then(function(listOfEvents) {
-      res.status(200).send(listOfEvents);
+    .then(function(listOfPolls) {
+      res.status(200).send(listOfPolls);
     });
   }
 };
