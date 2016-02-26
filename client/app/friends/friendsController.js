d9a5501554131cb3918eb5c925a556ac874f0b58
Michael Cheung <michaelc1993@gmail.com>
2016-02-26 13:18:47 -0800

    implement friendsController and friendwView closes #52 closes #53

--- client/app/friends/friendsController.js
+++ client/app/friends/friendsController.js
@@ -1,10 +1,45 @@
 // friends controller
 angular.module('pollster.friend', [])
 
-.controller('FriendsController', function ($scope, Friend) {
-  $scope.friends = [];
-  $scope.userId = 0;
-  $scope.getFriends = function() {
-    $scope.friends = Friend.getFriends();
+.controller('FriendsController', function ($scope, $window, Friend) {
+  //var to hold the current users id from the the localStorage
+  var userId = $window.localStorage.getItem('com.id');
+
+  //[input] none
+  //[output] none
+  //[side effects] initializes the friendsView to show list of all friends.
+  var init = function() {
+    $scope.getFriends = Friend.getFriendsFromDb(userId)
+    .then(function() {
+      $scope.allFriends = Friend.getFriends();
+    });
+  }
+
+  init();
+
+  //[input] the id of the friend to be added.
+  //[output] none
+  //[side effects] adds the friend to the current users friend list.
+  $scope.addFriend = function(friendId) {
+    Friend.addFriendship(userId,friendId)
+    .then(function() {
+      init();
+    });
+  };
+
+  //[input] none
+  //[output] none
+  //[side effects] shows and hides the list of all users on click
+  $scope.clicked = false;
+  $scope.getUsers = function() {
+    if(!$scope.clicked) {
+      $scope.clicked = true;
+    } else {
+      $scope.clicked = false;
+    }
+    Friend.getAllUsersFromDb()
+    .then(function() {
+      $scope.allUsers = Friend.getUsers();
+    });
   };
 });
