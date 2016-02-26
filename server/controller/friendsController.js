a2de96c4317fc2404600e81c6ed169971d03d74d
Brian Ronaghan <brian.ronaghan@gmail.com>
2016-02-26 15:37:18 -0800

    adds documentation, removes logs

--- server/controller/friendsController.js
+++ server/controller/friendsController.js
@@ -2,8 +2,9 @@ var db = require('../db');
 
 module.exports = {
   // [input] recieves userId and friendId on req.body
-  // [output] returns the entry
+  // [output] none
   // [side effects] adds a friend relationship into Relationships
+  // ALSO adds to UserPoll join table all polls associated with the other user
   post: function(req, res) {
     var user1 = Number(req.body.userId);
     var user2 = Number(req.body.friendId);
@@ -11,6 +12,7 @@ module.exports = {
       UserId: user1,
       FriendId: user2
     })
+    // after creating the friend relationship, find all polls associated with user 2
     .then(function (user) {
       return db.models.UserPoll.findAll({
         where: {
@@ -18,6 +20,7 @@ module.exports = {
         }
       });
     })
+    // taking all polls associated with user 2, create an array of objects that have user1's id and the given poll id
     .then(function (listOfPollAssociationsToAdd) {
       return listOfPollAssociationsToAdd.map(function (pollAssociation) {
         return {
@@ -26,34 +29,38 @@ module.exports = {
         };
       });
     })
+    // taken that list of user Poll associations between user1 and all of user2's polls, create them in the UserPoll join table
     .then(function (newPairs) {
       return db.models.UserPoll.bulkCreate(newPairs);
     })
+    // after creating relationships between user1 and user2's polls, do the opposite:
     .then(function (newRels) {
+      // find all pollIds already associated with user 2
       var excludePolls = newRels.map (function (pollRel) {
         return pollRel.PollId;
       });
-      var test2 =  db.models.UserPoll.findAll({
+      // find all polls associated with user 1 THAT HAVEN'T already been associated with user 2
+      var pollsOfUser1 =  db.models.UserPoll.findAll({
         where: {
           UserId: user1,
           PollId: {$notIn: excludePolls}
         }
       });
-      console.log('from newRels test2', test2);
-      return test2;
+      return pollsOfUser1;
     })
+    // then, take the new poll associations, and create new UserPoll objects for each
     .then(function (otherPollAssociations) {
-      var test = otherPollAssociations.map(function (newPollAssociation) {
+      var newUser2Pairs = otherPollAssociations.map(function (newPollAssociation) {
         return {
           UserId: user2,
           PollId: newPollAssociation.PollId
         };
       });
-      console.log('from otherPollAssociations', test);
-      return test;
+      return newUser2Pairs;
     })
-    .then(function (otherNewPairs) {
-      return db.models.UserPoll.bulkCreate(otherNewPairs);
+    // Then bulk create the new user 2 associations with any remaining user 1 polls
+    .then(function (newUser2Pairs) {
+      return db.models.UserPoll.bulkCreate(newUser2Pairs);
     })
     .then(function () {
       res.json(201);
